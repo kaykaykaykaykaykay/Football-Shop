d3f67da2ff71d1ee73e8bd8d328415fb2ab8eb2c
kaykaykaykaykaykay <khayrukartajaya360@gmail.com>
2025-09-24 11:19:22 +0700

    finished assigment 4, resolved merge conflict

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import show_main, create_product, show_product, show_xml, show_json, show_xml_by_id, show_json_by_id, register, login_user, logout_user, add_employee
+from .views import show_main, create_product, show_product, show_xml, show_json, show_xml_by_id, show_json_by_id, register, login_user, logout_user
 
 app_name = 'main'
 
@@ -14,5 +14,4 @@ urlpatterns = [
     path('register/', register, name='register'),
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
-    path('add_employee', add_employee,name='add_employee' ),
 ]
