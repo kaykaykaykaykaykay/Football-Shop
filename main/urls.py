55d8223cecf0cddbd23f257352030dc874184391
kaykaykaykaykaykay <khayrukartajaya360@gmail.com>
2025-10-01 11:43:35 +0700

    Finished assignment 5

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import show_main, create_product, show_product, show_xml, show_json, show_xml_by_id, show_json_by_id, register, login_user, logout_user
+from .views import show_main, create_product, show_product, show_xml, show_json, show_xml_by_id, show_json_by_id, register, login_user, logout_user, edit_product, delete_product
 
 app_name = 'main'
 
@@ -7,6 +7,8 @@ urlpatterns = [
     path('', show_main, name='show_main'),
     path("create-product/", create_product, name="create_product"),
     path("product/<uuid:id>/", show_product, name="show_product"),
+    path("product/<uuid:id>/edit/", edit_product, name="edit_product"),
+    path("product/<uuid:id>/delete/", delete_product, name="delete_product"),
     path("xml/", show_xml, name="show_xml"),
     path("json/", show_json, name="show_json"),
     path("xml/<uuid:id>/", show_xml_by_id, name="show_xml_by_id"),
